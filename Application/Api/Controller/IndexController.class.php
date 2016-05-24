b86cc97f009cbd792a3b189345f7d9420738c92b
程超 <chengchao@iiapple.com>
2016-05-24 11:08:25 +0800

    peizhi

--- Application/Api/Controller/IndexController.class.php
+++ Application/Api/Controller/IndexController.class.php
@@ -4,7 +4,8 @@ namespace Api\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-
-        
+        $userModel  =  M("user","","R_DB_CONFIG");
+        $userList = $userModel->where('user_id=4')->find();
+        echo json_encode($userList);
     }
 }
