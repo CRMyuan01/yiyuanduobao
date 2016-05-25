858fc345a6c3a5a8ae4ddc140f3445ace8216c96
ye3245 <ye3245@sina.com>
2016-05-25 10:50:46 +0800

    choujaing

--- Application/Api/Controller/IndexController.class.php
+++ Application/Api/Controller/IndexController.class.php
@@ -3,10 +3,34 @@
 namespace Api\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-    public function index(){
-        $userModel  =  M("user","","R_DB_CONFIG");
-        $userList = $userModel->where('user_id=4')->find();
-        echo json_encode($userList);
+    public function draw(){
+        $Model = new \Think\Model();
+        $proResult=$Model->query("SELECT * FROM product t WHERE t.`max_reserver_number`=t.`pending_count` and t.status=1");
+        foreach ($proResult as $key => $value) {
+        
+        	$record_table=D(record);
+        	$record_list=$record_table->where("product_id='".$value['product_code']."'")->select();
+        	$num=0;
+        	foreach ($record_list as $rec_key => $rec_value) {
+        		for ($i=0; $i < $rec_value['count']; $i++) { 
+        			
+        			$participate[$num]=$rec_value['user_id'];
+        			$num++;
+        		}
+        	}
+        	
+        	shuffle(shuffle($participate));
+        	$a=array_rand($participate);
+        	$update['winner_id']=$participate[$a];
+        	$update['status']=2;
+        	$pro_table=D(product);
+        	$pro_table->where('product_code='.$value['product_code'])->save($update);
+
+
+        	
+
+        }
+        
     }
     
 }
