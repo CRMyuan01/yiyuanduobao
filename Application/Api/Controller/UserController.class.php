e675cd3b96be94e965da0633e78240034fb3d6b1
yezhicai <ye3245@sina.com>
2016-05-30 14:26:11 +0800

    controller=>BaseController

--- Application/Api/Controller/UserController.class.php
+++ Application/Api/Controller/UserController.class.php
@@ -1,9 +1,9 @@
 <?php
 namespace Api\Controller;
 use Think\Controller;	
-class UserController extends Controller {
+class UserController extends BaseController {
      function  regedit(){
-     	$BaseObj=new BaseController;
+     	
      	$_POST['username']='yezhicai2';
      	$_POST['password']='yezhicai';
    		
@@ -21,18 +21,18 @@ class UserController extends Controller {
         $a=$table->add();
         $info['id']=$a;
        	if ($a) {
-       		$BaseObj=new BaseController;
-       		$BaseObj->renderJson(USER_REGEDIT_SUCCESS,'注册成功',$info);
+       		
+       		$this->renderJson(USER_REGEDIT_SUCCESS,'注册成功',$info);
        	}else{
-       		$BaseObj=new BaseController;
-       		$BaseObj->renderJson(USER_REGEDIT_ERROR,'注册失败');
+       		
+       		$this->renderJson(USER_REGEDIT_ERROR,'注册失败');
        	}
        
     }
 
     function login(){
        
-    	$BaseObj=new BaseController;
+    	
         $table=D('user');
         $info['user_name']=$_POST['username'];
         $info['password']=$_POST['password'];
@@ -43,15 +43,15 @@ class UserController extends Controller {
         }
         //判断密码是否正确
         if (md5($info['password'])==$result['0']['password']) {
-            $BaseObj->renderJson(USER_LOGIN_SUCCESS,'登陆成功',$result['0']);
+            $this->renderJson(USER_LOGIN_SUCCESS,'登陆成功',$result['0']);
         }else{
-            $BaseObj->renderJson(USER_LOGIN_PWDERROR,'密码错误');
+            $this->renderJson(USER_LOGIN_PWDERROR,'密码错误');
         }
      
 }
 
         function addrecord(){
-        	$BaseObj=new BaseController;
+        	
     		$info['product_id']=1000377;
     		$info['user_id']=267098;
     		$info['count']=6;
@@ -63,7 +63,7 @@ class UserController extends Controller {
     		//判断该用户购买量是否超过预约总量
     		if ($pro_info['pending_count']+$info['count']>$pro_info['max_reserver_number']) {
 
-    			$BaseObj->renderJson(USER_ADDRECORD_PROOVERMAX,'用户购买数量超过商品的最大预约数');
+    			$this->renderJson(USER_ADDRECORD_PROOVERMAX,'用户购买数量超过商品的最大预约数');
     		}else{
     			//添加预约信息
 				$b=$Record_obj->addRecord($info);
@@ -76,33 +76,33 @@ class UserController extends Controller {
 					}
 					//更新商品信息
 					$Product_obj->UpdateProductInfo($proToUpdate,$where);
-					$BaseObj->renderJson(USER_ADDRECORD_SUCCESS,'用户购买成功');
+					$this->renderJson(USER_ADDRECORD_SUCCESS,'用户购买成功');
 				}else{
-					$BaseObj->renderJson(USER_ADDRECORD_ERROR,'用户购买失败');
+					$this->renderJson(USER_ADDRECORD_ERROR,'用户购买失败');
 				}
     		}
 
         }
 		//获取所有展示商品
         function showprodect(){
-        	$BaseObj=new BaseController;
+        	
     		$Product_obj = new \Api\Model\ProductModel();
     		
     		
     		//获取商品信息
     		$pro_info=$Product_obj->getAllProduct();
     		if ($pro_info) {
-    			$BaseObj->renderJson(USER_SHOWPRODECT_SUCCESS,'所有商品获取成功',$pro_info);
+    			$this->renderJson(USER_SHOWPRODECT_SUCCESS,'所有商品获取成功',$pro_info);
     		}else{
 
-    			$BaseObj->renderJson(USER_SHOWPRODECT_ERROR,'所有商品获取失败');
+    			$this->renderJson(USER_SHOWPRODECT_ERROR,'所有商品获取失败');
     		}
     	
 
         }
         //添加购物车
         function addToBuyCar(){
-            $BaseObj=new BaseController;
+            
             $info=array('product_id'=>1000377,'user_id'=>267098,'count'=>3);
             $Buycar_obj = new \Api\Model\BuycarModel();
             //判断购物车里面是否已经有这个商品
@@ -115,16 +115,16 @@ class UserController extends Controller {
             }
 
             if ($returnNum) {
-                $BaseObj->renderJson(USER_ADDBUYCAR_SUCCESS,'购物车商品添加成功');
+                $this->renderJson(USER_ADDBUYCAR_SUCCESS,'购物车商品添加成功');
             }else{
-                $BaseObj->renderJson(USER_ADDBUYCAR_ERROR,'购物车商品添加失败');
+                $this->renderJson(USER_ADDBUYCAR_ERROR,'购物车商品添加失败');
             }
 
         }
 
 
         function showBuyCar(){
-            $BaseObj=new BaseController;
+            
             $info=array('user_id'=>267098);
             $Buycar_obj = new \Api\Model\BuycarModel();
             $buycarInfo=$Buycar_obj->selectinfo(array('user_id'=>$info['user_id']));
@@ -134,10 +134,10 @@ class UserController extends Controller {
                 $returnArr[$key]=array('count'=>$value['count'],'product_id'=>$value['product_id'],'user_id'=>$value['user_id'],'product_name'=>$pro_info['product_name'],'image_url'=>$pro_info['image_url']);
                 
             }
-            $BaseObj->renderJson(USER_SHOWBUYCAR_SUCCESS,'购物车展示成功',$returnArr);
+            $this->renderJson(USER_SHOWBUYCAR_SUCCESS,'购物车展示成功',$returnArr);
         }
         function payOfBuyCar(){
-            $BaseObj=new BaseController;
+            
             $info=array('product_id'=>'1000377,1000378','user_id'=>267098);
             //获取需要结账的商品id
             $product_id=explode(',',$info['product_id']);
@@ -162,33 +162,33 @@ class UserController extends Controller {
                 $b=$Record_obj->addRecord($rec_info);
             }
             if ($b) {
-                $BaseObj->renderJson(USER_PAYBUYCAR_SUCCESS,'购物车付款成功');
+                $this->renderJson(USER_PAYBUYCAR_SUCCESS,'购物车付款成功');
             }else{
-                $BaseObj->renderJson(USER_PAYBUYCAR_ERROR,'购物车付款失败');
+                $this->renderJson(USER_PAYBUYCAR_ERROR,'购物车付款失败');
             }
           
             
         }
         function updateCountBuyCar(){
-            $BaseObj=new BaseController;
+            
             $info=array('product_id'=>1000377,'user_id'=>267098,'count'=>3);
             $Buycar_obj = new \Api\Model\BuycarModel();
             $returnNum=$Buycar_obj->updateinfo(array('count'=>$info['count']),array('product_id'=>$info['product_id'],'user_id'=>$info['user_id']));
             if ($returnNum) {
-                $BaseObj->renderJson(USER_UPDATECOUNTBUYCAR_SUCCESS,'商品数量更新成功');
+                $this->renderJson(USER_UPDATECOUNTBUYCAR_SUCCESS,'商品数量更新成功');
             }else{
-                $BaseObj->renderJson(USER_UPDATECOUNTBUYCAR_ERROR,'商品数量更新失败');
+                $this->renderJson(USER_UPDATECOUNTBUYCAR_ERROR,'商品数量更新失败');
             }
         }
         function deleteBuyCar(){
-            $BaseObj=new BaseController; 
+            
             $info=array('product_id'=>1000377,'user_id'=>267098);
             $Buycar_obj = new \Api\Model\BuycarModel();
             $returnNum=$Buycar_obj->delinfo('product_id="'.$info['product_id'].'" and user_id="'.$info['user_id'].'"');
             if ($returnNum) {
-                $BaseObj->renderJson(USER_DELBUYCAR_SUCCESS,'购物车删除成功');
+                $this->renderJson(USER_DELBUYCAR_SUCCESS,'购物车删除成功');
             }else{
-                $BaseObj->renderJson(USER_DELBUYCAR_ERROR,'购物车删除成功');
+                $this->renderJson(USER_DELBUYCAR_ERROR,'购物车删除成功');
             }
         }
 
