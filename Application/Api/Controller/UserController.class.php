82a8fa6548b9070b052eac4c879942c40ae2e7b8
yezhicai <ye3245@sina.com>
2016-05-31 15:53:38 +0800

    gouwuche

--- Application/Api/Controller/UserController.class.php
+++ Application/Api/Controller/UserController.class.php
@@ -80,6 +80,7 @@ class UserController extends BaseController {
 				$b=$Record_obj->addRecord($info);
 				if ($b) {
 					$proToUpdate['pending_count']=$pro_info['pending_count']+$info['count'];
+                    $proToUpdate['storage']=$pro_info['storage']-$info['count'];
 					$where='product_code='.$info['product_id'];
 					//判断用户购买之后是否刚满预约总量,如果是更新字段status
 					if ($proToUpdate['pending_count']==$pro_info['max_reserver_number']) {
@@ -166,17 +167,44 @@ class UserController extends BaseController {
               $Buycar_obj = new \Api\Model\BuycarModel();
             $buycarInfo=$Buycar_obj->selectinfo($where);//查询购物车中该商品id的信息
             $delbuycarInfo=$Buycar_obj->delinfo($where);//删除购物车中该商品id的信息
-
             $Record_obj = new \Api\Model\RecordModel();
             //将商品信息插入购买记录表
             foreach ($buycarInfo as $key1 => $value1) {
-                $rec_info = array('user_id' =>$value1['user_id'] ,'product_id'=>$value1['product_id'],'count'=>$value1['count'] );
-                $b=$Record_obj->addRecord($rec_info);
-            }
-            if ($b) {
-                $this->renderJson(USER_PAYBUYCAR_SUCCESS,'购物车付款成功');
+
+
+
+                $info['product_id']=$value1['product_id'];
+                $info['user_id']=$value1['user_id'];
+                $info['count']=$value1['count'];
+                $Product_obj = new \Api\Model\ProductModel();
+            
+            //获取商品信息
+            $pro_info=$Product_obj->GetProductInfoByProid($info['product_id']);
+            //判断该用户购买量是否超过预约总量
+            if ($pro_info['pending_count']+$info['count']>$pro_info['max_reserver_number']) {
+
+                $this->renderJson(USER_ADDRECORD_PROOVERMAX,'用户购买数量超过商品的最大预约数');
             }else{
-                $this->renderJson(USER_PAYBUYCAR_ERROR,'购物车付款失败');
+                //添加预约信息
+                $b=$Record_obj->addRecord($info);
+                if ($b) {
+                    $proToUpdate['pending_count']=$pro_info['pending_count']+$info['count'];
+                    $proToUpdate['storage']=$pro_info['storage']-$info['count'];
+                    $where='product_code='.$info['product_id'];
+                    //判断用户购买之后是否刚满预约总量,如果是更新字段status
+                    if ($proToUpdate['pending_count']==$pro_info['max_reserver_number']) {
+                        $proToUpdate['status']=1;
+                    }
+                    //更新商品信息
+                    $Product_obj->UpdateProductInfo($proToUpdate,$where);
+                    
+                }
+                }
+            }
+
+
+            }
+                $this->renderJson(USER_PAYBUYCAR_SUCCESS,'购物车付款成功');
             }
           
             
