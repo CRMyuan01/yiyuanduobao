79756d79d1c590389ed8974ab9827fa2308a50f5
yezhicai <ye3245@sina.com>
2016-06-02 10:06:22 +0800

    1

--- Application/Api/Controller/BaseController.class.php
+++ Application/Api/Controller/BaseController.class.php
@@ -4,7 +4,7 @@ namespace Api\Controller;
 use Think\Controller;
 class BaseController extends Controller {
     public function renderJson($status,$msg,$info='',$recordid=''){//echo $msg;die;
-        $ReturnInfo=array('status'=>$status,'msg'=>$msg,'info'=>$info,'recordid'=$recordid);
+        $ReturnInfo=array('status'=>$status,'msg'=>$msg,'info'=>$info,'recordid'=>$recordid);
         echo json_encode($ReturnInfo);
         die;
     }
